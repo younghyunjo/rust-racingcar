f0810532bd2fd259241c682910d60b2575a67edf
Younghyun <y.jo@navercorp.com>
2022-05-12 14:48:59 +0900

    feat(output):add output

--- src/domain/position.rs
+++ src/domain/position.rs
@@ -18,6 +18,12 @@ impl From<u32> for Position {
     }
 }
 
+impl From<Position> for u32 {
+    fn from(p: Position) -> Self {
+        p.position
+    }
+}
+
 impl Position {
     pub fn new() -> Position {
         Position { position: 0 }
@@ -25,5 +31,9 @@ impl Position {
 
     pub fn increase(&self) -> Position {
         Position::from(self.position + 1)
+    }
+
+    pub fn position(&self) -> u32 {
+        self.position
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod view;
 mod domain;
 
 fn main() {
