137cd738dbb5da8a3edaf52e8c032ac1f77dfa33
Younghyun <y.jo@navercorp.com>
2022-05-23 14:49:54 +0900

    feat(name):add name module

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,6 +1,7 @@
 mod car;
 mod cars;
 pub mod judge;
+pub mod name;
 pub mod position;
 pub mod racing_game;
 pub mod racing_game_callbacks;
---
+++ src/domain/name.rs
@@ -0,0 +1,57 @@
+use std::fmt::Debug;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+#[error("{message:}")]
+pub struct NameError {
+    pub message: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Name {
+    name: String,
+}
+
+const MAX_NAME_LEN: usize = 5;
+
+impl Name {
+    pub fn new(name: &str) -> Result<Self, NameError> {
+        if name.len() > MAX_NAME_LEN {
+            return Err(NameError {
+                message: "name is too long".to_string(),
+            });
+        }
+        Ok(Name {
+            name: name.to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::name::Name;
+
+    #[test]
+    fn given_name_when_new_then_ok() {
+        //given
+        let name = "name";
+
+        //when
+        let ret = Name::new(name);
+
+        //then
+        assert_eq!(ret.is_ok(), true);
+    }
+
+    #[test]
+    fn given_long_name_when_new_then_err() {
+        //given
+        let name = "long_name";
+
+        //when
+        let ret = Name::new(name);
+
+        //thenx
+        assert_eq!(ret.is_err(), true);
+    }
+}
